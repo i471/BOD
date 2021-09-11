1b215a243c832f378118d9a0bb25dde38e7209b5
Robert G <robertdgil@hotmail.com>
2021-09-10 21:08:45 -0700

    Added Frenchie Video

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,24 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 import { Button } from './Button'
 import Video from '../assets/videos/travel.mp4'
+import Video2 from '../assets/videos/frenchieScratch.mp4'
 
 const Hero = () => {
     return (
         <HeroContainer>
             <HeroBg>
-                <VideoBg src={Video} type="video/mp4" autoPlay loop muted playsInline/>
+                <VideoBg src={Video2} type="video/mp4" autoPlay loop muted playsInline/>
 
 
             </HeroBg>
             <HeroContent>
                 <HeroItems>
                     <HeroH1>
-                        Unreal Destinations
+                        BullyOnDuty
                     </HeroH1>
                     <HeroP>
-                        Out of this world
+                        Your girlfriend's favorite breeder.
                     </HeroP>
-                    <Button>Travel Now</Button>
+                    <Button primary="true" big="true" round="true">Book Consult</Button>
                 </HeroItems>
             </HeroContent>
         </HeroContainer>
@@ -46,15 +47,47 @@ const HeroBg = styled.div`
     bottom:0;
     right: 0;
     left:0;
-    
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
 `
 
-const VideoBg = styled.video``
+const VideoBg = styled.video`
+    width: 100%;
+    height: 100%;
+    --o-object-fit: cover;
+    object-fit: cover;
+`
 
-const HeroContent = styled.div``
+const HeroContent = styled.div`
+    z-index:3;
+    height: calc(100vh - 80px);
+    max-height: 100%;
+    padding: 0rem calc((100vw - 1300px) / 2);
+`
 
-const HeroItems = styled.div``
+const HeroItems = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100vh;
+    max-height: 100%;
+    padding: 0;
+    color: #fff;
+    line-height: 1.1;
+    font-weight: bold;
+`
 
-const HeroH1 = styled.h1``
+const HeroH1 = styled.h1`
+    font-size: clamp(1.5rem, 6vw, 4rem); //Adds dynamic sizing based on view port
+    margin-bottom: 1.5rem;
+    letter-spacing: 3px;
+    padding: 0 1rem;
+`
 
-const HeroP = styled.p``
+const HeroP = styled.p`
+    font-size: clamp(1rem,3vw,3rem);
+    margin-bottom: 2rem;
+`
